8a3217a97a1124476ce8527a12ef2cad9e3a0d59
stuart-bennett <stuart.bennett@live.co.uk>
2017-08-04 22:01:50 +0100

    Initial work around Spotify fetch calls
    Started implementing the actual http calls to Spotify API

--- react-mixtape/src/spotifyApi.js
+++ react-mixtape/src/spotifyApi.js
@@ -1,7 +1,19 @@
+const apiBaseUrl = "https://api.spotify.com";
+
+type SearchResponse = {
+    title: string
+};
+
+const doSearch = (searchTerm: string): Promise<SearchResponse> =>
+    fetch(`${apiBaseUrl}/search?type=tracks&q=${searchTerm}`)
+        .then(r => r.json())
+        .then(j => ({ title: j.title }));
+
 const search = (searchTerm: string):Promise<Array<string>> =>
-    Promise.resolve([
+    doSearch(searchTerm).then(r => [
         searchTerm + "#1",
-        searchTerm + "#2"
+        searchTerm + "#2",
+        r.title
     ]);
 
 export { search };

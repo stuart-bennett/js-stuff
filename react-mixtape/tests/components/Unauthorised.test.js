30045c85240b57a170506a9eb9fe98b54ac94df7
stuart-bennett <stuart.bennett@live.co.uk>
2017-08-09 19:37:04 +0100

    Update test expectation
    The expectation on the OAuth authorize link needed updating

--- react-mixtape/tests/components/Unauthorised.test.js
+++ react-mixtape/tests/components/Unauthorised.test.js
@@ -5,7 +5,7 @@ import Unauthorised from "components/Unauthorised"
 describe("Unauthorised component", () => {
     test("should render", () => {
         const clientId = "test.client";
-        const expected = `https://accounts.spotify.com?client_id=${clientId}&response_type=token&redirect_uri=http://localhost:8080/webpack-dev-server/bundle`;
+        const expected = `https://accounts.spotify.com/authorize/?client_id=${clientId}&response_type=token&redirect_uri=http://localhost:8080/bundle`;
         const component = shallow(<Unauthorised clientId={clientId} />);
         const $a = component.find("a");
         expect($a.props()).toHaveProperty("href", expected);

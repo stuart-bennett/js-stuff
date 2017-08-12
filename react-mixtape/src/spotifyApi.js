8273bae1acc846cc709a40f0ecb4aba4a15e1d2f
stuart-bennett <stuart.bennett@live.co.uk>
2017-08-12 20:28:16 +0100

    Add getPlaylists method
    Implement access to /me/playlists endpoint

--- react-mixtape/src/spotifyApi.js
+++ react-mixtape/src/spotifyApi.js
@@ -9,10 +9,22 @@ const search = (searchTerm: string, token: string): Promise<Either<string, Array
     `${apiBaseUrl}/search?type=track&q=${searchTerm}`,
     new Headers({ "Authorization": "Bearer " + token }));
 
-const searchMap = (a: ApiResponse => Array<SearchResult>) =>
+const getPlaylists = (token: string): Promise<Either<string, Array<Playlist>>> => fetchMap(
+    playlistMap,
+    `${apiBaseUrl}/me/playlists`,
+    new Headers({ "Authorization": "Bearer " + token }));
+
+const playlistMap = (a: GetPlaylistsResponse => Array<Playlist>) =>
+    a.items.map(x => ({
+        id: x.id,
+        name: x.name,
+        images: x.images
+    }));
+
+const searchMap = (a: SearchResponse => Array<SearchResult>) =>
     a.tracks.items.map(x => ({
         id: x.id,
         title: x.name
     }));
 
-export { search };
+export { search, getPlaylists };

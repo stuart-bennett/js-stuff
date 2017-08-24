5ef3a0b9e19f5aefaa6126c55ef4a75bcd03588c
stuart-bennett <stuart.bennett@live.co.uk>
2017-08-24 21:07:30 +0100

    Update expected URL to include scope and new redirect uri

--- react-mixtape/tests/components/Unauthorised.test.js
+++ react-mixtape/tests/components/Unauthorised.test.js
@@ -5,7 +5,7 @@ import Unauthorised from "components/Unauthorised"
 describe("Unauthorised component", () => {
     test("should render", () => {
         const clientId = "test.client";
-        const expected = `https://accounts.spotify.com/authorize/?client_id=${clientId}&response_type=token&redirect_uri=http://localhost:8080/bundle`;
+        const expected = `https://accounts.spotify.com/authorize/?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=http://localhost:8080/index.html`;
         const component = shallow(<Unauthorised clientId={clientId} />);
         const $a = component.find("a");
         expect($a.props()).toHaveProperty("href", expected);

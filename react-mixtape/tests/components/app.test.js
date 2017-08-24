a16bff9d6d05f381674da9d43963654bd48eb52f
stuart-bennett <stuart.bennett@live.co.uk>
2017-08-24 22:17:36 +0100

    Specify a mock value for location.hash

--- react-mixtape/tests/components/app.test.js
+++ react-mixtape/tests/components/app.test.js
@@ -1,7 +1,11 @@
+import fetchMock from 'jest-fetch-mock';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from 'components/app';
 
+// Provide mock implementation for Fetch API
+window.fetch = fetchMock;
+window.location.hash = "#access_token=test.token";
 test('App component works', () => {
     const app = renderer.create(<App />);
     expect(app).not.toBeNull();

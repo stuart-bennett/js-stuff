450cef7a397050fd58af57366d008d01a7adbf63
stuart-bennett <stuart.bennett@live.co.uk>
2017-12-20 20:36:50 +0000

    Renamed "app" component to "App"

--- react-mixtape/src/app.js
+++ react-mixtape/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'components/app';
+import App from 'components/App';
 
 const init = ($mount: HTMLElement) => {
     const $root = document.createElement('div');

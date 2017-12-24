75a7e276c5b38814c5116e8a8203eb5c09572f88
stuart-bennett <stuart.bennett@live.co.uk>
2017-12-24 19:50:32 +0000

    Make empty options more explicit

--- react-mixtape/src/store.js
+++ react-mixtape/src/store.js
@@ -1,7 +1,9 @@
 import { createStore } from 'redux'
 import { devToolsEnhancer } from 'redux-devtools-extension'
 import reducer from 'reducers'
+
+const EmptyOptions = {};
 export default createStore(
     reducer,
-    devToolsEnhancer({})
+    devToolsEnhancer(EmptyOptions)
 );

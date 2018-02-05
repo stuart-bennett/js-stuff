b9786dc2fe56436995c43138c523d09565049600
stuart-bennett <stuart.bennett@live.co.uk>
2018-02-05 21:06:09 +0000

    App -> Functional Component

--- react-mixtape/src/components/redux/App.js
+++ react-mixtape/src/components/redux/App.js
@@ -8,34 +8,19 @@ import Unauthorised from 'components/redux/Unauthorised'
 type Props = {
     auth: Authentication
 };
-type State = {};
 
 const clientId: string = "1f662e1ad1ae494382cd56133ebb7b14";
 
-class App extends React.Component<Props, Props, State> {
-    static defaultProps = {
-        auth: { isAuthenticated: false },
-    };
+const App = (props: Props) => {
 
-    state = {};
-
-    constructor(props: Props) {
-        super(props);
-    }
-
-    componentDidMount() {
-    }
-
-    render() {
-        return this.props.auth.isAuthenticated
-            ? <AuthApp
-                user={this.props.auth.user}
-                token={this.props.auth.token} />
-            : <Unauthorised clientId={clientId} />
-    }
+    return props.auth.isAuthenticated
+        ? <AuthApp
+            user={props.auth.user}
+            token={props.auth.token} />
+        : <Unauthorised clientId={clientId} />
 }
 
-function mapStateToProps(state): State {
+function mapStateToProps(state): Props {
     console.log(state);
     return {
         auth: state.authentication

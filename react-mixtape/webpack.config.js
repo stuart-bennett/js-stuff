b90dedfc43615424fd706225ef306cab65dc41cc
stuart-bennett <stuart.bennett@live.co.uk>
2018-05-05 12:47:22 +0100

    Add webpack-clean-plugin & clean out dist/ dir pre-build

--- react-mixtape/webpack.config.js
+++ react-mixtape/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const CleanWebPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     plugins: [
+        new CleanWebPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: "React Mixtape fkjds"
         })

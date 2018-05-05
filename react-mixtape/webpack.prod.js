5d6f7aa2e4cae88a8ad52739764952b9b1c991da
stuart-bennett <stuart.bennett@live.co.uk>
2018-05-05 18:22:48 +0100

    Remove uglifyjs from prod
    Don't think uglifyjs is needed (or should be used this way) now that Babel is being used

--- react-mixtape/webpack.prod.js
+++ react-mixtape/webpack.prod.js
@@ -6,9 +6,6 @@ const webpack = require('webpack');
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
-        new uglifyJSPlugin({
-            sourceMap: true
-        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })

608cab4cb4d3f9d65ba549722314e916a9137e63
stuart-bennett <stuart.bennett@live.co.uk>
2018-05-05 12:43:28 +0100

    Add HtmlWebpackPlugin
    Have webpack create index.html instead of a static one

--- react-mixtape/webpack.config.js
+++ react-mixtape/webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "React Mixtape fkjds"
+        })
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')

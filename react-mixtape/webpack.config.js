c2a7a6e52c6d72f934b9b57639675d426ae5096c
stuart-bennett <stuart.bennett@live.co.uk>
2018-05-05 16:39:31 +0100

    Use hot module replacement

--- react-mixtape/webpack.config.js
+++ react-mixtape/webpack.config.js
@@ -1,19 +1,22 @@
 const path = require('path');
 const CleanWebPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     devtool: 'inline-source-maps',
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
     },
     plugins: [
         new CleanWebPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: "React Mixtape fkjds"
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output: {
         filename: 'main.js',

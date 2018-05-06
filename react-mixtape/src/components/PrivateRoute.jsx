12be7deda9237cec3c3f44ba4bc6fe4bfda97b59
stuart-bennett <stuart.bennett@live.co.uk>
2018-05-06 17:37:46 +0100

    PrivateRoute component
    Inspired by the component shown in the React-Router docs, this component
    ensures that it's components do not render if the auth implementation
    says that the user is not authenticated.
    
    The intention is that the interface works identical to the standard
    <Route /> component.
    
    Auth implementation is currently provided by utils/fakeAuth.js

---
+++ react-mixtape/src/components/PrivateRoute.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as auth from '../utils/fakeAuth';
+
+class PrivateRoute extends React.Component {
+
+    componentWillMount() {
+        auth.registerOnChange((isAuthenticated) => this.setState({
+            isAuthenticated
+        }));
+
+        this.setState({
+            isAuthenticated: auth.isAuthenticated()
+        })
+    }
+
+    render() {
+        if (this.state.isAuthenticated) {
+            return <Route {...this.props} />;
+        }
+
+        return null;
+    }
+}
+
+PrivateRoute.propTypes = {
+    component: PropTypes.func.isRequired
+};
+
+export default PrivateRoute;

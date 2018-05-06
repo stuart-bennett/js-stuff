2b95517c727627cdbdb09017e5c9d2a96dbfbd93
stuart-bennett <stuart.bennett@live.co.uk>
2018-05-06 17:42:34 +0100

    Add login functionality to App component
    Using the new login code (PrivateRoute component, Login container/component & fakeAuth
    implementation) - provide a means of only showing logged-in related
    components if the user is logged in. Also hooks up the <Login />
    component to use the fake "login()" function to emulate being logged in

--- react-mixtape/src/components/App.jsx
+++ react-mixtape/src/components/App.jsx
@@ -1,14 +1,15 @@
-/* eslint-disable no-console */
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import { Route } from 'react-router-dom';
+import LoginContainer from '../containers/LoginContainer.jsx';
 import PlaylistsContainer from '../containers/PlaylistsContainer.jsx';
 import PlaylistContainer from '../containers/PlaylistContainer.jsx';
+import PrivateRoute from './PrivateRoute.jsx';
 
 const App = () => (
 <div>
-    <PlaylistsContainer />
-    <Route path="/:playlistId" component={PlaylistContainer} />
+    <LoginContainer />
+    <PrivateRoute path="/" component={PlaylistsContainer} />
+    <PrivateRoute path="/:playlistId" component={PlaylistContainer} />
 </div>
 );
 

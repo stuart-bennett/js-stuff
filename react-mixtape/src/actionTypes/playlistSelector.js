0ea1f41102055d7bdc69e8bdec51ab0de955dc84
stuart-bennett <stuart.bennett@live.co.uk>
2018-05-10 18:40:40 +0100

    Add new tryLogin action

--- react-mixtape/src/actionTypes/playlistSelector.js
+++ react-mixtape/src/actionTypes/playlistSelector.js
@@ -1,3 +1,7 @@
-export const FETCH_PLAYLISTS = "fetchPlaylists";
-export const FETCH_PLAYLISTS_SUCCESS = "fetchPlaylistsSuccess";
-export const FETCH_PLAYLISTS_FAIL = "fetchPlaylistsFail";
+// Auth
+export const TRY_LOGIN = 'tryLogin';
+
+// Playlists
+export const FETCH_PLAYLISTS = 'fetchPlaylists';
+export const FETCH_PLAYLISTS_SUCCESS = 'fetchPlaylistsSuccess';
+export const FETCH_PLAYLISTS_FAIL = 'fetchPlaylistsFail';

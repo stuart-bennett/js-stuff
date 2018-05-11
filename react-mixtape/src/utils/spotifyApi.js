f047a8bbf0e02121f6fdcdae9ca81e94d487e856
Stuart Bennett <stuart.bennett@live.co.uk>
2018-05-11 16:14:29 +0100

    Use TLS Spotify API endpoint

--- react-mixtape/src/utils/spotifyApi.js
+++ react-mixtape/src/utils/spotifyApi.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://api.spotify.com/v1";
+const baseUrl = "https://api.spotify.com/v1";
 
 const createHeaders = (authToken) => new Headers({
     'Authorization': `Bearer ${authToken}`

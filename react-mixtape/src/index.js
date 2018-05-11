490a537579bf614064347f051b84280ee2cde54d
Stuart Bennett <stuart.bennett@live.co.uk>
2018-05-11 22:33:56 +0100

    Added normalize.css dependency

--- react-mixtape/src/index.js
+++ react-mixtape/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'normalize.css/normalize.css';
 
 const $root = document.createElement('div');
 document.documentElement.appendChild($root);

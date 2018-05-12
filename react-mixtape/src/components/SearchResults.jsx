a9e943fa8da1f9fddd675edb8989441236723599
Stuart Bennett <stuart.bennett@live.co.uk>
2018-05-12 17:36:15 +0100

    Initial styling of search results

--- react-mixtape/src/components/SearchResults.jsx
+++ react-mixtape/src/components/SearchResults.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './searchResults.css';
+
+const getPrimaryArtist = artists => artists[0].name;
 
 const SearchResults = ({ results }) => (
-    <ul>
-        { results.map(r => <li key={r.id}>{r.songTitle}</li>) }
+    <ul className={styles.container}>
+        { results.map(r =>
+            <li key={r.id}>
+                <img src={r.image} alt={`Track artwork for ${r.songTitle}`} />
+                <div className={styles.details}>
+                    <h2 className={styles.songName}>{r.songTitle}</h2>
+                    <h3 className={styles.artist}>{ getPrimaryArtist(r.artists) }</h3>
+                </div>
+            </li>
+        )}
     </ul>
 );
 

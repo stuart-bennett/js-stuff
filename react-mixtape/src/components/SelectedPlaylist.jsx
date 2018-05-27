3db2d556a2d938f12521dba5efe79f1bc743c307
stuart-bennett <stuart.bennett@live.co.uk>
2018-05-27 23:10:55 +0100

    Style the selected playlist header

--- react-mixtape/src/components/SelectedPlaylist.jsx
+++ react-mixtape/src/components/SelectedPlaylist.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Icon } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import PropTypes from 'prop-types';
+import styles from './selectedPlaylist.css';
 
 const SelectedPlaylist = ({
     name,
@@ -12,14 +13,12 @@ const SelectedPlaylist = ({
     image,
     isPublic
 }) => (
-<header>
-    <image src={image} alt={`Cover image for playlist '${name}'`} />
-    <div>
-        <h1>{name}</h1>
-        <span>
-            Has <strong>{ numberOfFollowers }</strong> followers
-        </span>
-    </div>
+<header className={styles.container}>
+    <img src={image} alt={`Cover image for playlist '${name}'`} />
+    <h1>{name}</h1>
+    <span>
+        Has <strong>{ numberOfFollowers }</strong> followers
+    </span>
     <ul>
         <li>
             <Icon icon={IconNames.LINK} />

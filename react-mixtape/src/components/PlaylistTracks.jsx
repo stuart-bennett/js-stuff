7f2506bdf55c5563845c69527ab286cdce2d62ce
stuart-bennett <stuart.bennett@live.co.uk>
2018-05-27 23:07:14 +0100

    Styling for playlist tracks

--- react-mixtape/src/components/PlaylistTracks.jsx
+++ react-mixtape/src/components/PlaylistTracks.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './playlistTracks.css';
 
 const PlaylistTracks = ({ tracks }) => (
-    <div>
+    <div className={styles.container}>
+        <h1>Tracklisting</h1>
         <ol>
         { tracks.map((t, i) => <li key={`${t.id}_${i}`}>
                 <div>

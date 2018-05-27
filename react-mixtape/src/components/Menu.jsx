ddc969e9e296d48239ee45612d65126112581c37
Stuart Bennett <stuart.bennett@live.co.uk>
2018-05-27 22:18:29 +0100

    Update Login component to use new user properties
    Having pulled in the user's profile image, their profile URL and their
    number of followers, use them in the user component to add a more personalised feel to the app.

--- react-mixtape/src/components/Menu.jsx
+++ react-mixtape/src/components/Menu.jsx
@@ -1,21 +1,51 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styles from './menu.css';
+import { Icon } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import Spinner from './Spinner.jsx';
+import styles from './menu.css';
 
-const Menu = ({ userId }) => {
+const Menu = ({ userId, profileImage, profileUrl, numberOfFollowers }) => {
     if (userId == null) {
         return <Spinner />;
     }
 
     return (
         <div className={styles.container}>
-            Logged in as { userId }
+            <div className={styles.profile}>
+                <img
+                    className={styles.profileImage}
+                    src={ profileImage }
+                    alt={ `Profile image for Spotify User ${userId}` }
+                />
+                <div className={styles.stats}>
+                    <span className={styles.userName}>{ userId }</span>
+                    <dl>
+                        <dt>No. of Followers</dt>
+                        <dd className={styles.numberofFollowers}>
+                            <Icon
+                                icon={IconNames.FOLLOWER}
+                                title="Number of Followers" />
+                            { numberOfFollowers }
+                        </dd>
+                        <dt>Profile URL</dt>
+                        <dd className={styles.profileUrl}>
+                            <Icon
+                                icon={IconNames.LINK}
+                                title="Spotify Profile" />
+                            <a href={ profileUrl }>{ profileUrl }</a>
+                        </dd>
+                    </dl>
+                </div>
+            </div>
         </div>);
 };
 
 Menu.propTypes = {
-    userId: PropTypes.string
+    userId: PropTypes.string,
+    profileImage: PropTypes.string.isRequired,
+    profileUrl: PropTypes.string,
+    numberOfFollowers: PropTypes.number
 };
 
 export default Menu;

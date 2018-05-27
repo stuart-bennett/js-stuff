0ace405042f9a1c394f317f9af8e3ea8d032380e
stuart-bennett <stuart.bennett@live.co.uk>
2018-05-27 22:26:32 +0100

    Add image to playlist selection

--- react-mixtape/src/components/PlaylistSelector.jsx
+++ react-mixtape/src/components/PlaylistSelector.jsx
@@ -7,8 +7,14 @@ const PlaylistSelector = ({ playlists }) => (
     <div className={styles.container}>
         <h1>Your Playlists</h1>
         <menu>
-        { playlists.map(pl => <div key={pl.id}>
-            <Link to={pl.id}>{pl.name}</Link></div>) }
+            <ul>
+            { playlists.map(pl => <li key={pl.id}>
+                <Link to={pl.id}>
+                    <img src={pl.image} />
+                    {pl.name}
+                </Link>
+            </li>) }
+            </ul>
         </menu>
     </div>
 );

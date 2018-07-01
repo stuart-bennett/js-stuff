358358fd1f21bfa0f72c0ca5fd93e97abba30b7f
stuart-bennett <stuart.bennett@live.co.uk>
2018-07-01 19:17:33 +0100

    Add better name to addTracks action creator test

--- react-mixtape/src/actions/playlist.test.js
+++ react-mixtape/src/actions/playlist.test.js
@@ -2,11 +2,13 @@ import { addTrack } from './playlist';
 import * as actions from '../actionTypes';
 
 describe("playlist action creators", () => {
-    test("test", () => {
-        const track = {};
-        expect(addTrack(track)).toEqual({
-            type: actions.PLAYLIST_TRACKS_ADD,
-            track
+    describe("calling addTrack", () => {
+        test("creates the action: PLAYLIST_TRACKS_ADD", () => {
+            const track = {};
+            expect(addTrack(track)).toEqual({
+                type: actions.PLAYLIST_TRACKS_ADD,
+                track
+            });
         });
     });
 });
